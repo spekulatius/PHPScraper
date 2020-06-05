d49bc7d0793bb403b7f23117e5647ebbd919c82e
Peter Thaleikis <peter.thaleikis@gmail.com>
2020-06-05 12:51:10 +0400

    MINOR: fixing and improving tests a little

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -29,8 +29,14 @@ final class BasicTest extends TestCase
         $web->go('https://test-pages.phpscraper.de');
 
         // Both the method call as well as property call should return the same...
-        $this->assertSame("PHP Scraper - An oppinated web-scraper library for PHP", $web->title);
-        $this->assertSame("PHP Scraper - An oppinated web-scraper library for PHP", $web->title());
+        $this->assertSame(
+            "PHP Scraper - An opinionated web-scraper library for PHP",
+            $web->title
+        );
+        $this->assertSame(
+            "PHP Scraper - An opinionated web-scraper library for PHP",
+            $web->title()
+        );
 
         // so...
         $this->assertSame($web->title, $web->title());
@@ -47,17 +53,26 @@ final class BasicTest extends TestCase
         $web->go('https://test-pages.phpscraper.de');
 
         // Both the method call as well as property call should return the same...
-        $this->assertSame("PHP Scraper - An oppinated web-scraper library for PHP", $web->title);
+        $this->assertSame(
+            "PHP Scraper - An opinionated web-scraper library for PHP",
+            $web->title
+        );
 
 
         // Leave the current page and head on to the next one.
         $web->go('https://github.com');
 
         // Shouldn't match, because we surfed on...
-        $this->assertNotSame("PHP Scraper - An oppinated web-scraper library for PHP", $web->title);
+        $this->assertNotSame(
+            "PHP Scraper - An opinionated web-scraper library for PHP",
+            $web->title
+        );
 
         // Instead it should be GitHub now.
         // Side-note: The not signed-in title for GitHub is different.
-        $this->assertSame("The world’s leading software development platform · GitHub", $web->title);
+        $this->assertSame(
+            "The world’s leading software development platform · GitHub",
+            $web->title
+        );
     }
 }
