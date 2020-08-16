e91bce24e4b53d9a1ef19b3f1ded97627eb2076e
Peter Thaleikis <peter.thaleikis@gmail.com>
2020-08-16 18:12:25 +0400

    MINOR: keyword scores

--- tests/KeywordTest.php
+++ tests/KeywordTest.php
@@ -9,7 +9,7 @@ final class KeywordTest extends TestCase
     /**
      * @test
      */
-    public function testExtractionExamples()
+    public function testKeywordExtraction()
     {
         $web = new \spekulatius\phpscraper();
 
@@ -42,6 +42,46 @@ final class KeywordTest extends TestCase
         // check if all are part of the output
         foreach ($shouldKeywords as $keyword) {
             $this->assertTrue(in_array($keyword, $keywords));
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function testKeywordExtractionWithScores()
+    {
+        $web = new \spekulatius\phpscraper();
+
+        // Navigate to the test page.
+        // It contains 3 paragraphs from the English Wikipedia article for "lorem ipsum"
+        $web->go('https://test-pages.phpscraper.de/content/keywords.html');
+
+        // Check the keywords on this case...
+        $keywords = $web->contentKeywordsWithScores;
+
+        // a selected list of keywords to expect
+        $shouldKeywords = [
+            '1960s' => 1.0,
+            'added' => 1.0,
+            'adopted lorem ipsum' => 11.0,
+            'advertisements' => 1.0,
+            'aldus employed' => 4.0,
+            'corrupted version' => 4.0,
+            'graphic' => 1.0,
+            'improper latin' => 4.0,
+            'introduced' => 1.0,
+            'keyword extraction tests' => 9.0,
+            'test' => 1.0,
+            'microsoft word' => 5.3333333333333,
+            'english wikipedia' => 4.0,
+            'lorem ipsum' => 8.0,
+            'lorem ipsum text' => 11.0,
+        ];
+
+        // check if all are part of the output with the expected score
+        foreach ($shouldKeywords as $keyword => $score) {
+            // has the same score
+            $this->assertSame($keywords[$keyword], $score);
         }
     }
 }
--- websites/examples/extract-keywords.md
+++ websites/examples/extract-keywords.md
@@ -68,3 +68,52 @@ foreach ($keywords as $keyword) {
 ::: tip
 The default language (locale) for this is `en_US`. Other languages can be passed as a parameter. This currently works only for a selection of languages. Check this [list](https://github.com/Donatello-za/rake-php-plus#currently-supported-languages) for further information.
 :::
+
+
+## Scoring of Keywords
+
+Not every keyword has the same weight in the ranking-algorithms of search engines. A mix of several factors and SEO-signals decides on the weight a search engine assigns to a word. Frequency of words, length of the texts, and variations such as synonyms can lead to different weighting.
+
+The PHPScraper library allows you to get an indication of keyword weights in the form of scores:
+
+
+```PHP
+$web = new \spekulatius\phpscraper();
+
+// Navigate to the test page.
+// It contains 3 paragraphs from the English Wikipedia article for "lorem ipsum"
+$web->go('https://test-pages.phpscraper.de/content/keywords.html');
+
+// check the number of keywords.
+$keywords = $web->contentKeywordsWithScores;
+echo "This page contains at least " . count($keywords) . " keywords/phrases.\n\n";
+
+// Loop through the keywords
+foreach ($keywords as $keyword => $score) {
+    echo sprintf(" - %s (%s)\n", $keyword, $score);
+}
+
+/**
+ * Will print out:
+ *
+ * This page contains at least 40 keywords/phrases.
+ *
+ * [...]
+ *  - 1960s (1.0)
+ *  - added (1.0)
+ *  - adopted lorem ipsum (11.0)
+ *  - advertisements (1.0)
+ *  - aldus employed (4.0)
+ *  - corrupted version (4.0)
+ *  - graphic (1.0)
+ *  - improper latin (4.0)
+ *  - introduced (1.0)
+ *  - keyword extraction tests (9.0)
+ *  - test (1.0)
+ *  - microsoft word (5.3333333333333)
+ *  - english wikipedia (4.0)
+ *  - lorem ipsum (8.0)
+ *  - lorem ipsum text (11.0)
+ * [...]
+ */
+```
