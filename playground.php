bb4a01e98563e94020f13e0de672705a1d363fcd
Peter Thaleikis <peter.thaleikis@gmail.com>
2022-09-21 18:51:19 +0200

    Adding playground

---
+++ playground.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+echo "\n";
+echo "#########################\n";
+echo "# PHPScraper Playground #\n";
+echo "#########################\n";
+echo "\n";
+
+$web = new \spekulatius\phpscraper;
