a422a0aca4d753e28bdcbd45ba99665188ee5216
Peter Thaleikis <peter.thaleikis@gmail.com>
2022-10-14 11:40:07 +0200

    Split files while keeping the history

--- src/Core.php
+++ src/Core.php
@@ -1,71 +1,15 @@
 <?php
 
-// Namespace
 namespace spekulatius;
 
-// Libs used.
-// https://github.com/FriendsOfPHP/Goutte
 use Goutte\Client;
-
-// https://github.com/jeremykendall/php-domain-parser
 use Pdp\Cache;
 use Pdp\CurlHttpClient;
 use Pdp\Manager;
-
-// https://github.com/Donatello-za/rake-php-plus
 use DonatelloZa\RakePlus\RakePlus;
-
-// https://symfony.com/doc/current/http_client.html
 use Symfony\Component\HttpClient\HttpClient;
 
-class phpscraper
-{
-    /**
-     * Holds the client
-     *
-     * @var spekulatius\core;
-     */
-    protected $core = null;
-
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->core = new core();
-    }
-
-    /**
-     * Catch alls to properties and process them accordingly.
-     *
-     * @param string $name
-     * @return mixed
-     */
-    public function __get(string $name)
-    {
-        // We are assuming that all calls for properties actually method calls...
-        return $this->call($name);
-    }
-
-    /**
-     * Catches the method calls and tries to satisfy them.
-     *
-     * @param string $name
-     * @param array $arguments = null
-     * @return mixed
-     */
-    public function __call(string $name, array $arguments = null)
-    {
-        if ($name == 'call') {
-            $name = $arguments[0];
-            return $this->core->$name();
-        } else {
-            return $this->core->$name(...$arguments);
-        }
-    }
-}
-
-class core
+class Core
 {
     /**
      * Holds the client
