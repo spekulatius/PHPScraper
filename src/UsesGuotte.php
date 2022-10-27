2ca34caae75e634442daf9c4f886060e41ba8911
Peter Thaleikis <peter.thaleikis@gmail.com>
2022-10-27 22:33:36 +0200

    Splitting Core into multiple traits for easier extendability

---
+++ src/UsesGuotte.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace spekulatius;
+
+use Goutte\Client as GoutteClient;
+use Symfony\Component\DomCrawler\Crawler;
+
+trait UsesGuotte
+{
+    /**
+     * Holds the client
+     *
+     * @var Goutte\Client
+     */
+    protected $client = null;
+
+    /**
+     * Holds the current page (a Crawler object)
+     *
+     * @var \Symfony\Component\DomCrawler\Crawler
+     */
+    protected $currentPage = null;
+
+    /**
+     * Overwrites the client
+     *
+     * @param \Goutte\Client $client
+     */
+    public function setClient(GoutteClient $client): self
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    /**
+     * Returns the current url
+     *
+     * @return string $url
+     */
+    public function currentURL(): string
+    {
+        return $this->currentPage->getUri();
+    }
+
+    /**
+     * Navigates to an url
+     *
+     * @param string $url
+     */
+    public function go(string $url): self
+    {
+        $this->currentPage = $this->client->request('GET', $url);
+
+        return $this;
+    }
+
+    /**
+     * Allows to set HTML content to process.
+     *
+     * @param string $url
+     * @param string $content
+     */
+    public function setContent(string $url, string $content): self
+    {
+        $this->currentPage = new Crawler($content, $url);
+
+        return $this;
+    }
+
+    /**
+     * Fetch an asset from a given URL (for example an image)
+     *
+     * @param string $url
+     */
+    public function fetchAsset(string $url)
+    {
+        return $this->client->request('GET', $url)->getResponse();
+    }
+
+    /**
+     * Click a link (either with title or url)
+     *
+     * @param string $titleOrUrl
+     * @return boolean
+     */
+    public function clickLink($titleOrUrl)
+    {
+        // If the string starts with http just go to it - we assume it's an URL
+        if (\stripos($titleOrUrl, 'http') === 0) {
+            // Go to a URL
+            $this->go($titleOrUrl);
+        } else {
+            // Find link based on the title
+            $link = $this->currentPage->selectLink($titleOrUrl)->link();
+
+            // Click the link and store the DOMCrawler object
+            $this->currentPage = $this->client->click($link);
+        }
+
+        return true;
+    }
+}
