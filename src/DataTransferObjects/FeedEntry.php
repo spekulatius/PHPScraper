61fd28e93c4f80d233853e7e5c6d792805d73e4c
Peter Thaleikis <peter.thaleikis@gmail.com>
2022-11-07 13:11:58 +0100

    Tidy up DTO

--- src/DataTransferObjects/FeedEntry.php
+++ src/DataTransferObjects/FeedEntry.php
@@ -20,13 +20,9 @@ class FeedEntry
     ) {
     }
 
-    // This could be saved with a package, but this would install a lot of Laravel stuff too...
-    public static function fromArray(array $data)
+    public static function fromArray(array $data): self
     {
-        // Ensure the URL is absolute.
-
-
-        // Convert to an object.
+        // Convert to an object and return the instance.
         return new self(
             $data['title'] ?? '',
             $data['description'] ?? '',
