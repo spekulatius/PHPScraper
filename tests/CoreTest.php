d01a1c6589d9ec3dbdc53233f8eddec8cfaaa3a2
Peter Thaleikis <peter.thaleikis@gmail.com>
2022-11-08 22:26:09 +0100

    Improve CoreTest: Decouple test from website title

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -27,24 +27,23 @@ class CoreTest extends \PHPUnit\Framework\TestCase
     {
         $web = new \spekulatius\phpscraper;
 
-        // Navigate to test page
+        // 1. Navigate to test page
         $web->go('https://test-pages.phpscraper.de/meta/lorem-ipsum.html');
 
         // Both the method call as well as property call should return the same...
-        $this->assertSame(
-            'Lorem Ipsum',
-            $web->title
-        );
+        $this->assertSame('https://test-pages.phpscraper.de/meta/lorem-ipsum.html', $web->currentUrl);
+        $this->assertSame('Lorem Ipsum', $web->title);
 
 
-        // Leave the current page and head on to the next one.
+        // 2. Leave the current page and head on to the next one.
         $web->go('https://phpscraper.de');
 
+        // We should have navigated.
+        $this->assertSame('https://phpscraper.de', $web->currentUrl);
+
         // Shouldn't match, because we surfed on...
-        $this->assertNotSame(
-            'Lorem Ipsum',
-            $web->title
-        );
+        $this->assertNotSame('https://test-pages.phpscraper.de/meta/lorem-ipsum.html', $web->currentUrl);
+        $this->assertNotSame('Lorem Ipsum', $web->title);
     }
 
     /**
