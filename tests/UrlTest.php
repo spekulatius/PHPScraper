8601141e68638950b49ffe08c65ff2773125f951
Peter Thaleikis <peter.thaleikis@gmail.com>
2022-11-08 22:44:48 +0100

    Improve test outputs

--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -19,13 +19,13 @@ class UrlTest extends \PHPUnit\Framework\TestCase
         $web->go('https://test-pages.phpscraper.de/content/lists.html');
 
         // Ensure the URL is set correctly.
-        $this->assertSame($web->currentUrl, 'https://test-pages.phpscraper.de/content/lists.html');
+        $this->assertSame('https://test-pages.phpscraper.de/content/lists.html', $web->currentUrl);
 
         // Ensure the host is parsed correctly.
-        $this->assertSame($web->currentHost, 'test-pages.phpscraper.de');
+        $this->assertSame('test-pages.phpscraper.de', $web->currentHost);
 
         // Ensure the host with protocol is parsed correctly.
-        $this->assertSame($web->currentBaseUrl, 'https://test-pages.phpscraper.de');
+        $this->assertSame('https://test-pages.phpscraper.de', $web->currentBaseUrl);
     }
 
     /**
@@ -41,32 +41,32 @@ class UrlTest extends \PHPUnit\Framework\TestCase
         // Test variations of paths to be processed
         // With leading slash
         $this->assertSame(
-            $web->makeUrlAbsolute('/index.html'),
             'https://phpscraper.de/index.html',
+            $web->makeUrlAbsolute('/index.html'),
         );
 
         // Without leading slash
         $this->assertSame(
-            $web->makeUrlAbsolute('index.html'),
             'https://phpscraper.de/index.html',
+            $web->makeUrlAbsolute('index.html'),
         );
 
         // Paths are considered.
         $this->assertSame(
-            $web->makeUrlAbsolute('test/index.html'),
             'https://phpscraper.de/test/index.html',
+            $web->makeUrlAbsolute('test/index.html'),
         );
 
         // Absolutely URLs are untouched.
         $this->assertSame(
-            $web->makeUrlAbsolute('https://example.com/index.html'),
             'https://example.com/index.html',
+            $web->makeUrlAbsolute('https://example.com/index.html'),
         );
 
         // Protocol is considered
         $this->assertSame(
-            $web->makeUrlAbsolute('http://example.com/index.html'),
             'http://example.com/index.html',
+            $web->makeUrlAbsolute('http://example.com/index.html'),
         );
     }
 
@@ -83,32 +83,32 @@ class UrlTest extends \PHPUnit\Framework\TestCase
         // Test variations of paths to be processed
         // With leading slash
         $this->assertSame(
-            $web->makeUrlAbsolute('/index.html', 'https://example.com'),
             'https://example.com/index.html',
+            $web->makeUrlAbsolute('/index.html', 'https://example.com'),
         );
 
         // Without leading slash
         $this->assertSame(
-            $web->makeUrlAbsolute('index.html', 'https://example.com'),
             'https://example.com/index.html',
+            $web->makeUrlAbsolute('index.html', 'https://example.com'),
         );
 
         // Paths are considered.
         $this->assertSame(
-            $web->makeUrlAbsolute('test/index.html', 'https://example.com'),
             'https://example.com/test/index.html',
+            $web->makeUrlAbsolute('test/index.html', 'https://example.com'),
         );
 
         // Absolutely URLs are untouched.
         $this->assertSame(
-            $web->makeUrlAbsolute('https://example.com/index.html', 'https://example.com/test/with/path'),
             'https://example.com/index.html',
+            $web->makeUrlAbsolute('https://example.com/index.html', 'https://example.com/test/with/path'),
         );
 
         // Protocol is considered
         $this->assertSame(
-            $web->makeUrlAbsolute('http://example.com/index.html', 'https://example.com/test/with/path'),
             'http://example.com/index.html',
+            $web->makeUrlAbsolute('http://example.com/index.html', 'https://example.com/test/with/path'),
         );
     }
 }
