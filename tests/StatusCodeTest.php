fa1ca44171d61d4084e31f2fb1d7d94efbacf91b
eposjk <eposjk@users.noreply.github.com>
2022-12-12 01:49:24 +0100

    enhanced status code tests

--- tests/StatusCodeTest.php
+++ tests/StatusCodeTest.php
@@ -24,8 +24,8 @@ class StatusCodeTest extends \PHPUnit\Framework\TestCase
     {
         $web = new \Spekulatius\PHPScraper\PHPScraper;
 
-        // Navigate to the test page: This redirects to phpscraper.de
-        $web->go('https://phpscraper.de');
+        // Navigate to the test page without redirect
+        $web->go('https://phpscraper.de/');
 
         // Check the status itself.
         $this->assertSame(200, $web->statusCode);
@@ -34,6 +34,14 @@ class StatusCodeTest extends \PHPUnit\Framework\TestCase
         $this->assertTrue($web->isSuccess);
         $this->assertFalse($web->isClientError);
         $this->assertFalse($web->isServerError);
+        $this->assertFalse($web->isTemporaryResult);
+        $this->assertFalse($web->isGone);
+        $this->assertFalse($web->isPermanentError);
+
+        // Check the request properties
+        $this->assertFalse($web->usesTemporaryRedirect);
+        $this->assertSame('', $web->permanentRedirectUrl);
+        $this->assertSame(0, $web->retryAt);
 
         // Assert access-helpers
         $this->assertFalse($web->isForbidden);
@@ -57,9 +65,145 @@ class StatusCodeTest extends \PHPUnit\Framework\TestCase
         $this->assertFalse($web->isSuccess);
         $this->assertTrue($web->isClientError);
         $this->assertFalse($web->isServerError);
+        $this->assertFalse($web->isTemporaryResult);
+        $this->assertFalse($web->isGone);
+        $this->assertTrue($web->isPermanentError);
+
+        // Check the request properties
+        $this->assertFalse($web->usesTemporaryRedirect);
+        $this->assertSame('', $web->permanentRedirectUrl);
+        $this->assertSame(0, $web->retryAt);
 
         // Assert access-helpers
         $this->assertFalse($web->isForbidden);
         $this->assertTrue($web->isNotFound);
     }
+
+    /**
+     * @test
+     */
+    public function testPermanentRedirect()
+    {
+        $web = new \Spekulatius\PHPScraper\PHPScraper;
+
+        // Navigate to the test page with 301 permanent redirect
+        $web->go('http://phpscraper.de/');
+
+        // Check the status itself.
+        $this->assertSame(200, $web->statusCode);
+
+        // Check the detailed states.
+        $this->assertTrue($web->isSuccess);
+        $this->assertFalse($web->isClientError);
+        $this->assertFalse($web->isServerError);
+        $this->assertFalse($web->isTemporaryResult);
+        $this->assertFalse($web->isGone);
+        $this->assertFalse($web->isPermanentError);
+
+        // Check the request properties
+        $this->assertFalse($web->usesTemporaryRedirect);
+        $this->assertSame('https://phpscraper.de/', $web->permanentRedirectUrl);
+        $this->assertSame(0, $web->retryAt);
+
+        // Assert access-helpers
+        $this->assertFalse($web->isForbidden);
+        $this->assertFalse($web->isNotFound);
+    }
+
+    /**
+     * @test
+     */
+    public function testTemporaryRedirect()
+    {
+        $web = new \Spekulatius\PHPScraper\PHPScraper;
+
+        // Navigate to the test page with 307 temporary redirect
+        $web->go('https://httpstat.us/307');
+
+        // Check the status itself.
+        $this->assertSame(200, $web->statusCode);
+
+        // Check the detailed states.
+        $this->assertTrue($web->isSuccess);
+        $this->assertFalse($web->isClientError);
+        $this->assertFalse($web->isServerError);
+        $this->assertTrue($web->isTemporaryResult);
+        $this->assertFalse($web->isGone);
+        $this->assertFalse($web->isPermanentError);
+
+        // Check the request properties
+        $this->assertTrue($web->usesTemporaryRedirect);
+        $this->assertSame('', $web->permanentRedirectUrl);
+        $this->assertSame(0, $web->retryAt);
+
+        // Assert access-helpers
+        $this->assertFalse($web->isForbidden);
+        $this->assertFalse($web->isNotFound);
+    }
+
+    /**
+     * @test
+     */
+    public function testGone()
+    {
+        $web = new \Spekulatius\PHPScraper\PHPScraper;
+
+        // Navigate to the test page
+        $web->go('https://httpstat.us/410');
+
+        // Check the status itself.
+        $this->assertSame(410, $web->statusCode);
+
+        // Check the detailed states.
+        $this->assertFalse($web->isSuccess);
+        $this->assertTrue($web->isClientError);
+        $this->assertFalse($web->isServerError);
+        $this->assertFalse($web->isTemporaryResult);
+        $this->assertTrue($web->isGone);
+        $this->assertTrue($web->isPermanentError);
+
+        // Check the request properties
+        $this->assertFalse($web->usesTemporaryRedirect);
+        $this->assertSame('', $web->permanentRedirectUrl);
+        $this->assertSame(0, $web->retryAt);
+
+        // Assert access-helpers
+        $this->assertFalse($web->isForbidden);
+        $this->assertFalse($web->isNotFound);
+    }
+
+    /**
+     * @test
+     */
+    public function testTooManyRequests()
+    {
+        $web = new \Spekulatius\PHPScraper\PHPScraper;
+
+        // Navigate to the test page which returns "429 Too Many Requests" with "Retry-At: 5" header
+        $t1 = time();
+        $web->go('https://httpstat.us/429');
+        $t2 = time();
+
+        // Check the status itself.
+        $this->assertSame(429, $web->statusCode);
+
+        // Check the detailed states.
+        $this->assertFalse($web->isSuccess);
+        $this->assertTrue($web->isClientError);
+        $this->assertFalse($web->isServerError);
+        $this->assertTrue($web->isTemporaryResult);
+        $this->assertFalse($web->isGone);
+        $this->assertFalse($web->isPermanentError);
+
+        // Check the request properties
+        $this->assertFalse($web->usesTemporaryRedirect);
+        $this->assertSame('', $web->permanentRedirectUrl);
+        $this->assertGreaterThan($t1, $web->retryAt);
+        $this->assertLessThanOrEqual($t2 + 5, $web->retryAt);
+
+        // Assert access-helpers
+        $this->assertFalse($web->isForbidden);
+        $this->assertFalse($web->isNotFound);
+    }
+
 }
