f4f7d8436016ba959d69ce61092f7204c88683cd
Peter <peter.thaleikis@gmail.com>
2023-08-29 13:06:44 +0200

    Tweak rules for current state

--- rector.php
+++ rector.php
@@ -17,10 +17,9 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-        SetList::CODE_QUALITY,
+        // LevelSetList::UP_TO_PHP_82,
+        // SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
         SetList::TYPE_DECLARATION,
     ]);
 };
